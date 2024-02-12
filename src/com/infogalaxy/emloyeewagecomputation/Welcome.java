13af41fa7c14609748004471fd7c76815145d122
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-02-12 21:33:46 +0530

    [ADDED] : YOGESH Employee Wages Computation Setup

---
+++ src/com/infogalaxy/emloyeewagecomputation/Welcome.java
@@ -0,0 +1,10 @@
+package com.infogalaxy.emloyeewagecomputation;
+
+public class Welcome {
+	
+	public static void main(String args[])
+	{
+		System.out.print("Welcome to Employee Wage Computation");
+	}
+
+}
---
+++ src/com/infogalaxy/emloyeewagecomputation/package-info.java
@@ -0,0 +1,1 @@
+package com.infogalaxy.emloyeewagecomputation;
