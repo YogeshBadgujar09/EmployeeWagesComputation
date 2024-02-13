0cf7907d43ed793e89c201458a0d38d4aca73b52
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-02-13 20:24:01 +0530

    [ADDED] : YOGESH UC-2-Calculate Employee Wage Is completed

--- src/com/infogalaxy/emloyeewagecomputation/Welcome.java
+++ src/com/infogalaxy/emloyeewagecomputation/Welcome.java
@@ -9,6 +9,9 @@ public class Welcome {
 		System.out.println("Welcome to Employee Wage Computation");
 		
 		int attendance = 0;
+		int dailyWage = 0;
+		final int WAGE_PER_HR = 20;
+		final int FULL_DAY_HR = 8;
 		
 		Random random = new Random();
 	    attendance = random.nextInt(9)%2;
@@ -21,6 +24,8 @@ public class Welcome {
 		else
 		{
 			System.out.println("Employee is Present");
+			dailyWage = WAGE_PER_HR * FULL_DAY_HR ;
+			System.out.print("Employee Wage :" + dailyWage);
 		}
 		
 		
