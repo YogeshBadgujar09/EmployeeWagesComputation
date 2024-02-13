bd49c60a58fd5dc67c4a98fe0a79c4c4e6186661
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-02-13 11:37:33 +0530

    [MODIFIED] : YOGESH UC-1-Completed for  Employee Attendence

--- src/com/infogalaxy/emloyeewagecomputation/Welcome.java
+++ src/com/infogalaxy/emloyeewagecomputation/Welcome.java
@@ -1,10 +1,31 @@
 package com.infogalaxy.emloyeewagecomputation;
 
+import java.util.Scanner;
+
 public class Welcome {
 	
 	public static void main(String args[])
 	{
-		System.out.print("Welcome to Employee Wage Computation");
+		System.out.println("Welcome to Employee Wage Computation");
+		
+		int attendance = 0;
+		
+		System.out.print("Enter a Employee Attendance [0 - Absent / 1 - Present] :");
+		Scanner obj = new Scanner(System.in);
+		attendance = obj.nextInt();
+		
+		if(attendance == 0)
+		{
+			System.out.println("Employee is Absend");
+		}
+		else
+		{
+			System.out.println("Employee is Present");
+		}
+		
+		
+	
+		
 	}
 
 }
