90fb6d3be682707f5319b8d80b66684f4a64a2ef
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-02-14 16:11:34 +0530

    [MODIFIED] : YOGESH Add Employee Type Fuctionality

--- src/com/infogalaxy/emloyeewagecomputation/Welcome.java
+++ src/com/infogalaxy/emloyeewagecomputation/Welcome.java
@@ -8,10 +8,12 @@ public class Welcome {
 	{
 		System.out.println("Welcome to Employee Wage Computation");
 		
-		int attendance = 0;
-		int dailyWage = 0;
+		int attendance ;
+		int dailyWage ;
+		int employee_Type ;
 		final int WAGE_PER_HR = 20;
-		final int FULL_DAY_HR = 8;
+		final int PART_TIME_HR = 4;
+		final int FULL_TIME_HR = 8;
 		
 		Random random = new Random();
 	    attendance = random.nextInt(9)%2;
@@ -24,8 +26,21 @@ public class Welcome {
 		else
 		{
 			System.out.println("Employee is Present");
-			dailyWage = WAGE_PER_HR * FULL_DAY_HR ;
+			employee_Type = random.nextInt(9)%2;
+			if(employee_Type==0)
+			{
+				System.out.println("Employee is Part Time");
+				dailyWage = WAGE_PER_HR * PART_TIME_HR ;
+			}
+			else
+			{
+				System.out.println("Employee is Full Time");
+				dailyWage = WAGE_PER_HR * FULL_TIME_HR ;
+			}
+			
 			System.out.print("Employee Wage :" + dailyWage);
+
+ 			
 		}
 		
 		
