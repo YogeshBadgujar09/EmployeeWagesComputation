1d7d0f3af23e9e9884bf49ce00ea4960e8fcdbad
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-02-18 23:04:47 +0530

    [MODIFIED] : MonthlyWage Calculate Perfactly Refactor code

--- src/com/infogalaxy/emloyeewagecomputation/EmployeeWageComp.java
+++ src/com/infogalaxy/emloyeewagecomputation/EmployeeWageComp.java
@@ -9,7 +9,8 @@ public class EmployeeWageComp {
 		System.out.println("Welcome to Employee Wage Computation");
 		
 		int attendance ;
-		int dailyWage = 0 ;
+		int partTimer_dailyWage = 0 ;
+		int fullTimer_dailyWage = 0 ;
 		int employee_Type ;
 		final int WAGE_PER_HR = 20;
 		final int PART_TIME_HR = 4;
@@ -17,8 +18,9 @@ public class EmployeeWageComp {
 		int monthltAtten_Part_time_Emp = 0;
 		int monthltAtten_Full_time_Emp = 0;
 		int monthlyWage = 0;
+		final int workingDay = 20;
 
-		for (int i=1 ;i<=20 ;i++)
+		for (int i=1 ;i <= workingDay ; i++)
 		{
 			System.out.println("Day :"+i);
 
@@ -41,58 +43,48 @@ public class EmployeeWageComp {
 					{
 						case 0 :
 						System.out.println("Employee is Part Time");
-						dailyWage = PART_TIME_HR * WAGE_PER_HR ;
-						System.out.println("Employee Wage is :" + dailyWage );
+						partTimer_dailyWage = PART_TIME_HR * WAGE_PER_HR ;
+						System.out.println("Employee Wage is :" + partTimer_dailyWage );
 						monthltAtten_Part_time_Emp+=1;
-		//				monthlyWage = monthltAtten_Part_time_Emp * dailyWage ;  
+					//	monthlyWage = monthltAtten_Part_time_Emp * partTimer_dailyWage ;  
 					 	break;
 
 						case 1 :
 						System.out.println("Employee is Full Time");
-						dailyWage = FULL_TIME_HR * WAGE_PER_HR ;
-						System.out.println("Employee Wage is :" + dailyWage );
+						fullTimer_dailyWage = FULL_TIME_HR * WAGE_PER_HR ;
+						System.out.println("Employee Wage is :" + fullTimer_dailyWage);
 						monthltAtten_Full_time_Emp+=1;
-						monthlyWage = monthltAtten_Full_time_Emp * dailyWage ;  
+				//		monthlyWage = monthltAtten_Full_time_Emp * fullTimer_dailyWage  ;  
 						break;
 
-						default:
-						System.out.println("Case isn't match");
-						
+					 
 					}
 
 			break;
 
-			default:
-			System.out.println("Case isn't match");
-			
 			
 			}
 			
-		//	System.out.println("Employee Wage is :" + dailyWage );
-			System.out.println();
+			System.out.println("------------------------------");
 			
 			
 		}
-		
-		//System.out.println("Monthly Attendance of Full Time Employee :" + monthltAtten_Full_time_Emp+"\n"+ "Monthly Addence of Part Time Employee :" + monthltAtten_Part_time_Emp+"\n");
 		
 		Random random = new Random();
 		employee_Type =random.nextInt(3)%2;
 		
 		if(employee_Type==0)
 		{
-		monthlyWage = monthltAtten_Part_time_Emp * dailyWage ;
+		monthlyWage = monthltAtten_Part_time_Emp * partTimer_dailyWage ;
 		System.out.println("Monthly Addence of Part Time Employee :" + monthltAtten_Part_time_Emp+"\n"+"Part Timer Employee Salary :" + monthlyWage);
 		}
 		else
 		{
-		monthlyWage = monthltAtten_Full_time_Emp * dailyWage ;
+		monthlyWage = monthltAtten_Full_time_Emp * 	fullTimer_dailyWage ;
 		System.out.println("Monthly Attendance of Full Time Employee :" + monthltAtten_Full_time_Emp +"\n"+ "Full Timer Employee Salary :" + monthlyWage);
 		}
 
 		 
-		
-	 
 	}
 
 }
