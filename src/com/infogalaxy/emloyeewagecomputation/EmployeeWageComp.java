9a2469479c5bf1f42464771e9716c46d21904aaa
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-02-20 15:14:08 +0530

    [MODIFIED] : YOGESH calculate monthly Wage Using HOURS and Days

--- src/com/infogalaxy/emloyeewagecomputation/EmployeeWageComp.java
+++ src/com/infogalaxy/emloyeewagecomputation/EmployeeWageComp.java
@@ -11,15 +11,20 @@ public class EmployeeWageComp {
 		int attendance ;
 		int dailyWage = 0 ;
 		int employee_Type ;
-		int  totalWage= 0;
+		int totalWage= 0;
+		int working_Hr = 0;
+		int working_Day = 1;
 		final int WAGE_PER_HR = 20;
 		final int PART_TIME_HR = 4;
 		final int FULL_TIME_HR = 8;
- 		final int workingDay = 20;
+ 		final int MONTHLY_WORKIND_DAY = 20;
+ 		final int MINIMUM_WORKING_HR = 100;
 
-		for (int i=1 ;i <= workingDay ; i++)
+		//for (int i=1 ;i <= workingDay ; i++)
+		while(working_Hr < 50 && working_Day <= MONTHLY_WORKIND_DAY )
 		{
-			System.out.println("Day :"+i);
+			
+			System.out.println("Day :"+ working_Day );
 
 			Random random = new Random();
 			attendance = random.nextInt(9)%2;
@@ -42,12 +47,14 @@ public class EmployeeWageComp {
 						System.out.println("Employee is Part Time");
 						dailyWage = PART_TIME_HR * WAGE_PER_HR ;
 						System.out.println("Employee Wage is :" +  dailyWage );
+						working_Hr += PART_TIME_HR;
 					 	break;
 
 						case 1 :
 						System.out.println("Employee is Full Time");
 						dailyWage = FULL_TIME_HR * WAGE_PER_HR ;
-						System.out.println("Employee Wage is :" + dailyWage);																
+						System.out.println("Employee Wage is :" + dailyWage);
+						working_Hr += FULL_TIME_HR;
 						break;
 					}
 					break;
@@ -55,10 +62,14 @@ public class EmployeeWageComp {
 			}
 				
 			totalWage+=dailyWage;
+			System.out.println("Total Wage :" + totalWage); 
+			System.out.println("Total Working Hours :"+working_Hr);
 
 			System.out.println("------------------------------");
+			
+			 working_Day++;
 		}
-		
+		System.out.println("Total Working Hours In month :"+ working_Hr);
 		System.out.println("Total Monthly Wage :" + totalWage); 
 		 
 	}
